1fff246f1ea31d053d9a4bfcea9be34e9b3613db
Sabi Ku <sabiku85@gmail.com>
2025-12-04 10:34:55 +0100

    useHead composable

--- app/pages/chat.vue
+++ app/pages/chat.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
-const { chat, messages, sendMessage } = useChat()
+const { chat, messages, sendMessage } = useChat();
+
+useHead({
+  title: chat.value.title,
+  htmlAttrs: {
+    class: "my-unique-class",
+  },
+  script: [
+    {
+      innerHTML: 'window.analytics = window.analytics || ["hi"]',
+    },
+  ],
+});
 </script>
 
 <template>
-  <ChatWindow :chat :messages @send-message="sendMessage" />
+  <ChatWindow
+    :chat
+    :messages
+    @send-message="sendMessage"
+  />
 </template>
