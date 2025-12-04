01786f8ed3469cdf5b64de67ac7a07f093c103f3
Sabi Ku <sabiku85@gmail.com>
2025-12-04 20:20:05 +0100

    method

--- app/composables/useChat.ts
+++ app/composables/useChat.ts
@@ -1,38 +1,36 @@
-import type { Chat, ChatMessage } from '../types'
-import { MOCK_CHAT } from './mockData'
+import type { Chat, ChatMessage } from "../types";
+import { MOCK_CHAT } from "./mockData";
 
 export default function useChat() {
-  const chat = ref<Chat>(MOCK_CHAT)
-  const messages = computed<ChatMessage[]>(
-    () => chat.value.messages
-  )
+  const chat = ref<Chat>(MOCK_CHAT);
+  const messages = computed<ChatMessage[]>(() => chat.value.messages);
 
-  function createMessage(
-    message: string,
-    role: ChatMessage['role']
-  ) {
-    const id = messages.value.length.toString()
+  function createMessage(message: string, role: ChatMessage["role"]) {
+    const id = messages.value.length.toString();
 
     return {
       id,
       role,
       content: message,
-    }
+    };
   }
 
-  function sendMessage(message: string) {
-    messages.value.push(createMessage(message, 'user'))
+  async function sendMessage(message: string) {
+    messages.value.push(createMessage(message, "user"));
 
-    setTimeout(() => {
-      messages.value.push(
-        createMessage(`You said: ${message}`, 'assistant')
-      )
-    }, 200)
+    const data = await $fetch<ChatMessage>("/api/ai", {
+      method: "POST",
+      body: {
+        messages: messages.value,
+      },
+    });
+
+    messages.value.push(data);
   }
 
   return {
     chat,
     messages,
     sendMessage,
-  }
+  };
 }
